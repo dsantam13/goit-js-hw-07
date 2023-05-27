b9abf137d915173dae2c1e95a8c89ef7057f640a
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-05-27 18:07:52 -0500

    Homework 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryImages = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>
+    `).join("");
+
+gallery.innerHTML = galleryImages;
+
+const lightbox = new SimpleLightbox('.gallery__link', { 
+    captionsData: "alt",
+    captionDelay: 250
+});
+
