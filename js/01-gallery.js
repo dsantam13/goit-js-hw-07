20016d43661b7977c16c1786c47b4dbdae1b9c71
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-05-27 17:20:25 -0500

    Homework 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryImages = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`).join("");
+
+gallery.innerHTML = galleryImages;
+
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    } 
+    const instance = basicLightbox.create(`
+        <img src="${e.target.dataset.source}">
+    `);
+    instance.show();
+
+    gallery.addEventListener("keydown", (e) => {
+        if (e.code === "Escape") {
+            instance.close();
+        }
+    });
+}
+
+
